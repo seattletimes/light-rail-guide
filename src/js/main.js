28167ab438676b5e7161033dd905250117a6d376
Audrey Carlsen <acarlsen@seattletimes.com>
2016-12-05 15:31:32 -0800

    adds scroll to top buttons

--- src/js/main.js
+++ src/js/main.js
@@ -8,18 +8,16 @@ var animateScroll = require("./lib/animateScroll");
 var qsa = require("./lib/qsa");
 require("./lib/gallery.js");
 
-// qsa(".dot").forEach(function(d) {
-//   d.addEventListener("click", function() {
-//     var stop = d.getAttribute("data-id");
-//     console.log(stop)
-//     animateScroll("#" + stop);
-//   });
-// });
-
 document.querySelector(".nav-map svg").addEventListener("click", function(e) {
   if (e.target.classList[0].indexOf("cls-4") > -1) {
     var stop = e.target.parentNode.getAttribute("data-stop");
     console.log(e.target.parentNode.getAttribute("data-stop"))
     animateScroll("#" + stop)
   }
-})
+});
+
+qsa(".top-button").forEach(function(t) {
+  t.addEventListener("click", function(e) {
+    animateScroll("#top");
+  });
+});
