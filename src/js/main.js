51f44a62a6869138807d403514c51777078ec16e
Audrey Carlsen <acarlsen@seattletimes.com>
2016-12-05 15:05:38 -0800

    final galleries, nav map

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,18 @@ var animateScroll = require("./lib/animateScroll");
 var qsa = require("./lib/qsa");
 require("./lib/gallery.js");
 
-qsa(".dot").forEach(function(d) {
-  d.addEventListener("click", function() {
-    var stop = d.getAttribute("data-id");
-    console.log(stop)
-    animateScroll("#" + stop);
-  });
-});
+// qsa(".dot").forEach(function(d) {
+//   d.addEventListener("click", function() {
+//     var stop = d.getAttribute("data-id");
+//     console.log(stop)
+//     animateScroll("#" + stop);
+//   });
+// });
+
+document.querySelector(".nav-map svg").addEventListener("click", function(e) {
+  if (e.target.classList[0].indexOf("cls-4") > -1) {
+    var stop = e.target.parentNode.getAttribute("data-stop");
+    console.log(e.target.parentNode.getAttribute("data-stop"))
+    animateScroll("#" + stop)
+  }
+})
